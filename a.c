519eae6688f120415b06323a19267b1af552c208
Pasupathi <d.pasupathi@accenture.com>
2022-10-22 21:22:21 +0530

    initial draft program

---
+++ a.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "sys\time.h"
+
+int main(int argc, char const *argv[])
+{
+    int a=23;
+    int b=78;
+    printf("%d",a+b);
+    return 0;
+}
